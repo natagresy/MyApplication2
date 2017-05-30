6c4cf62297825ef8b1eb7ac7941c735c8100aa8f
natagresy <natagresy@gmail.com>
2017-05-30 16:36:37 +0700

    berhasil gabungin activity listlocation dan mapsactivity

--- app/src/main/java/com/example/user/myapplication/ListLocation.java
+++ app/src/main/java/com/example/user/myapplication/ListLocation.java
@@ -1,10 +1,12 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.user.myapplication.POJO.Example;
@@ -63,5 +65,8 @@ public class ListLocation extends AppCompatActivity {
             }
         });
     }
-
+    public void startMaps(View view)
+    {
+        startActivity(new Intent(this, MapsActivity.class));
+    }
 }
--- app/src/main/java/com/example/user/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/user/myapplication/MapsActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -37,7 +38,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
@@ -100,7 +101,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         //Initialize Google Play Services
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
