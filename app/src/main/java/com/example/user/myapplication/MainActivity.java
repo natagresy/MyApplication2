070976d9f79901cca313ac6dd03b5b95f105abf0
natagresy <natagresy@gmail.com>
2017-06-09 14:15:01 +0700

    add slider

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -6,8 +6,52 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity {
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static final Integer[] SLIDER= {R.drawable.bakery,R.drawable.bakmibaso,R.drawable.chinese,R.drawable.dessert,R.drawable.indonesian};
+    private ArrayList<Integer> SLIDERArray = new ArrayList<Integer>();
+
+    private void init() {
+        for(int i=0;i<SLIDER.length;i++)
+            SLIDERArray.add(SLIDER[i]);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new SlideAdapter(MainActivity.this,SLIDERArray));
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == SLIDER.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 2500, 2500);
+    }
+
+
+
+    //gridview buttons
     GridView grid;
     String[] web = {
             "Bakery",
@@ -38,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+        init();
         CustomGrid adapter = new CustomGrid(MainActivity.this, web, imageId);
         grid = (GridView) findViewById(R.id.grid);
         grid.setAdapter(adapter);
