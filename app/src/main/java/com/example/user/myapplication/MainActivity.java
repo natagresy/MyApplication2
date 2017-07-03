353b27008bf55bfb3315d5a47c0c8e26f8037a60
natagresy <natagresy@gmail.com>
2017-07-03 21:53:15 +0700

    rapiin az

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -37,11 +37,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     Location mLastLocation;
 
     private static int currentPage = 0;
-    private static final Integer[] SLIDER= {R.drawable.bakery,
-            R.drawable.bakmibaso,
-            R.drawable.chinese,
-            R.drawable.dessert,
-            R.drawable.indonesian};
+    private static final Integer[] SLIDER= {R.drawable.banner5,
+            R.drawable.banner1,
+            R.drawable.banner2,
+            R.drawable.banner3,
+            R.drawable.banner4};
     private ArrayList<Integer> SLIDERArray = new ArrayList<Integer>();
 
     private void init() {
